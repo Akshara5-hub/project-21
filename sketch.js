74783f5cce9ec7a650360161a131286a3f358799
Akshara5-hub <73822381+Akshara5-hub@users.noreply.github.com>
2020-12-20 13:02:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,7 @@ function setup() {
   createCanvas(1600,400);
 
 thickness=random(22, 83);
-speed=random(22, 55);
+speed=random(60, 160);
 weight=random(30, 52);
 
  bullet=createSprite(50, 200, 50, 10);
